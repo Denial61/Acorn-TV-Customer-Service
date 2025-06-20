aafce05d9c1e7463bb0c8e3da8851c791cb5b650
Denial61 <hs51525254@gmail.com>
2025-06-20 15:55:05 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+project = 'HitmanPro Login Account'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+
+   html_js_files = [
+    'chatbot.js',
+]
+
+html_favicon = '_static/download.png'
