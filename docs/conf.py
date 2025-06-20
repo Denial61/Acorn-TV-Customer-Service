f3e358edd3968a24e4fdba0a596e4eb949b43567
Denial61 <hs51525254@gmail.com>
2025-06-20 15:57:46 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
-project = 'HitmanPro Login Account'
-author = 'Your Name'
+project = 'Acorn TV Customer Service'
+author = 'Denial'
 release = '1.0'
 
 extensions = []
@@ -14,4 +14,4 @@ html_static_path = ['_static']
     'chatbot.js',
 ]
 
-html_favicon = '_static/download.png'
+html_favicon = '_static/https://img.utdstc.com/icon/842/cef/842ceff4223c4f530d0fe383310b4fec4a83acc42dd4be0345fd577089c82e2f:100'
